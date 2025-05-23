8faa95d6c5aa27d0ab1d0e73a6670ff31a2b92ce
Moon Productions <oudegraftak@proton.me>
2025-05-23 12:36:24 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+docker stop casa 2>/dev/null
+docker rm casa 2>/dev/null
+
+docker run -d \
+  --name casa \
+  -p ${SERVER_PORT}:80 \
+  -v casa_data:/mnt/server/casa/data \
+  dockur/casa
+
+echo "Container is running"
+docker logs -f casa
